3ab6ef934bdf979837e95f8a3707741412c439a8
Ross Westwater <ross.westwater@gmail.com>
2021-12-02 14:50:00 -0700

    adding query to edamam fetch request test in script.js.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // edit below apikey with anything that's relevant, if api key is required.
-var apikey = "c6GprvG8gXWujVItkIA58R1fhmYGlB5D";
+var appid = "0011fe5c";
+var apikey = "d275470047fa1b116d2b18e8130d5d08";
 
 var TESTFETCH = function (city) {
   // edit the below link with the fetch URL.
-  var runTest = `https://app.ticketmaster.com/discovery/v2/events.json?size=1&${apikey}`;
+  var runTest = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken%20breast&app_id=0011fe5c&app_key=d275470047fa1b116d2b18e8130d5d08&cuisineType=American&mealType=Dinner&random=true&field=label&field=image&field=ingredientLines&field=ingredients&field=calories`;
 
   fetch(runTest)
     .then(function (response) {
