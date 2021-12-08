fdc0304c0de0a106710e846fb982c0b309e1ec1d
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-12-07 23:05:50 -0700

    added modals and additional formating cleanup to seetGeak

--- assets/js/seatGeakLogic.js
+++ assets/js/seatGeakLogic.js
@@ -1,7 +1,9 @@
 // var myClientID ="MjQ3NDgwNzd8MTYzODUwMTM2OS43OTkxNDE0";
 // var mySID = "2b0b7028d8aead384e4849058a883ca9d344d06dc999989bcbee64d5e87255e3";
 
-
+var dialog = document.getElementById("dialogModal")
+var modalText = dialog.firstChild.nextSibling.firstChild.nextSibling
+console.log(modalText);
 var currentdate = new Date();
 var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth() + 1) + "-" + (currentdate.getDate()+1)
 
@@ -20,7 +22,9 @@ var getUserEvents = function () {
                     console.log(data);
                 });
             } else {
-                alert("input invalid");
+
+                modalText.textContent = "Looks like you forgot to enter your zip code or your desired search radius. Enter that information and we'll be able to suggest some events in your area!";
+                solveTheProblem();
                 return;
             }
         })
@@ -35,19 +39,20 @@ function renderItem(data) {
     var eventPosting = $(".eventPostersContainer");
     for (let i = 0; i < data.events.length; i++) {
 
-        var imageSection = $("<section></section>");
-        imageSection.attr("class", "col-3 px-5 py-2");
+        // var imageSection = $("<section></section>");
+        // imageSection.attr("class", "col-6 px-5 py-2");
         var linkEl = $("<a></a>");
 
         linkEl.attr("href", data.events[i].url);
         linkEl.attr("target", "_blank");
+        
 
         var imageEl = $("<img>").attr("id", "eventPoster");
         imageEl.attr("src", data.events[i].performers[0].image);
         imageEl.appendTo(linkEl);
         linkEl.appendTo(eventPosting);
 
-        var eventInfo = $("<div></div>").attr("class","eventInfo col-6");
+        var eventInfo = $("<div></div>").attr("class","eventInfo col-lg-8 col-sm-6");
 
         var eventDescriptEL = $("<h3></h3>").attr("id", "eventTitle card-title",);
         eventDescriptEL.text(data.events[i].title);
@@ -74,10 +79,6 @@ function renderItem(data) {
         saveButton.appendTo(eventInfo);
         eventInfo.appendTo(eventPosting);
 
-        
-
-        
-     
     }
 
 }
@@ -95,6 +96,16 @@ $(".eventInput").submit(function (event) {
 $("#saveTheEvent").on("click", function (event) {
     console.log(event)
     event.target;
-    
 
 })
+
+var cancelButton = document.getElementById('cancel')
+function solveTheProblem() {
+    dialog.showModal();
+    cancelButton.addEventListener('click', function() {
+        dialog.close();
+    });
+};
+
+
+
